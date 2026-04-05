edd4f441fa45e35744c03da328dfa44294a9e998
rogersau <rogers.man@gmail.com>
2026-04-05 16:00:12 +1000

    chore: align workspace bootstrap scripts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,21 @@ module.exports = {
     es2022: true,
     node: true,
   },
-  extends: ["eslint:recommended"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    project: false,
   },
   ignorePatterns: ["dist/", "coverage/", "node_modules/"],
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx", "vitest.workspace.ts"],
+      parserOptions: {
+        project: false,
+      },
+    },
+  ],
 };
